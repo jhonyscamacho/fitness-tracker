392404a11c35bbd6ce5d332ec696be45fbb84c82
Jhonys Camacho <jhonys.camacho@gmail.com>
2018-04-25 15:39:20 -0300

    Injects and uses the Training Service (Exercise Data).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,14 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
-
-import { AuthService } from './auth/auth.service';
 import { TrainingComponent } from './training/training.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
+
+import { AuthService } from './auth/auth.service';
+import { TrainingService } from './training/training.service';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,7 @@ import { StopTrainingComponent } from './training/current-training/stop-training
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [AuthService],
+  providers: [AuthService, TrainingService],
   bootstrap: [AppComponent],
   entryComponents: [StopTrainingComponent]
 })
--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,7 @@
+import { Exercise } from './../exercise.model';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { TrainingService } from './../training.service';
 
 @Component({
   selector: 'app-new-training',
@@ -7,10 +10,12 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class NewTrainingComponent implements OnInit {
   @Output() trainingStart = new EventEmitter<void>();
+  exercises: Exercise[] = [];
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
+    this.exercises = this.trainingService.getAvailableExercises();
   }
 
   onStartTraining() {
